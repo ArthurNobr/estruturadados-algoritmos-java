80b4e6897d23b3f4b03b7df2a01c14626792ce7b
ArthurNobr <arthurnunes.al@gmail.com>
2023-09-22 20:17:23 -0300

    Criação da classe vetor

---
+++ src/com/arthur/estruturadados/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.arthur.estruturadados.vetor;
+
+public class Vetor {
+
+    public String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+
+}
---
+++ src/com/arthur/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,13 @@
+package com.arthur.estruturadados.vetor.teste;
+
+import com.arthur.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+    }
+
+}
