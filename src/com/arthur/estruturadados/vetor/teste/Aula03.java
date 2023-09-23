6c2a4186a87c9f3cfd97978c53f46107d0b39ca8
ArthurNobr <arthurnunes.al@gmail.com>
2023-09-23 00:36:55 -0300

    Atualização classe vetor

---
+++ src/com/arthur/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package com.arthur.estruturadados.vetor.teste;
+
+import com.arthur.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+
+    }
+}
+
